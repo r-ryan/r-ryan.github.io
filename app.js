cd43371202a8e496fbe3f4ad1cf939b8da1bfcee
Ryanrayan500 <ryanrayan7@outlook.com>
2020-11-02 00:28:22 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//Movement Animation to happen 
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+//Moving Animation Event
+container.addEventlistener("mousemove", (e) => {
+	let xAxis = (window.innerWidth / 2 - e.pageX) /25;
+	let yAxis = (window.innerHeight / 2 - e.pageY) /25;
+	card.style.trasnform = `rotateY(${Yaxis}deg) rotateX(${xAxis}deg)`;
+} 
+
+console.log("thi is js")
+
+
+
